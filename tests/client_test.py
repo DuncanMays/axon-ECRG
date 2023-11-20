397535a2a30b64babc1872ba64ea4d93116ff4a6
DuncanMays <DuncanMays@outlook.com>
2023-11-19 22:39:18 -0500

    removed unused imports and expanded GenericStub tests

--- tests/client_test.py
+++ tests/client_test.py
@@ -16,19 +16,36 @@ async def test_RemoteWorker():
 	print(await w.rpcs.simplex_rpc('simplex test ', suffix='passed'))
 	print(await w.rpcs.duplex_rpc('duplex test ', suffix='passed'))
 
+	print(w.rpcs)
+
 	# tests that the ServiceStub is set up right
 	stub = w.simplex_service
 
 	# tests that child stubs are instantiated properly and that their RPCs work
 	for i in range(axon.config.default_service_depth, 0, -1):
+		await w.simplex_service()
+
+		w.simplex_service.sync_call((), {})
+		handle = w.simplex_service.async_call((), {})
+		handle.join()
+		await w.simplex_service.coro_call((), {})
+
 		await w.simplex_service.test_fn()
+
+		w.simplex_service.test_fn.sync_call((), {})
+		handle = w.simplex_service.test_fn.async_call((), {})
+		handle.join()
+		await w.simplex_service.test_fn.coro_call((), {})
+
+		if isinstance(w.simplex_service, axon.stubs.GenericStub):
+			print('Callable Stub inheritance from axon.stubs.GenericStub confirmed')
+		else:
+			raise BaseException('Callable Stub stub is not inheritance from axon.stubs.GenericStub')
 
 		if isinstance(w.simplex_service.test_fn, axon.stubs.GenericStub):
 			print('RPC inheritance from axon.stubs.GenericStub confirmed')
 		else:
 			raise BaseException('RPC stub is not inheritance from axon.stubs.GenericStub')
-
-		# idk how to tell if w.test is a ServiceStub
 
 		if (i != 1):
 			# if this is the last iteration, worker won't have a child and this line will raise an attribute error
