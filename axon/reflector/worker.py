9b5097d59191a9ebc804e2990e252bd20a224f35
Duncan <duncanmays@outlook.com>
2024-10-19 15:54:12 -0400

    implemented update_profile method on itlw

--- axon/reflector/worker.py
+++ axon/reflector/worker.py
@@ -5,6 +5,7 @@ from axon.transport_worker import invoke_RPC
 from axon.serializers import serialize, deserialize
 from axon.chunking import send_in_chunks, recv_chunks
 from axon.stubs import add_url_defaults
+from axon.worker import TLSNs
 
 from concurrent.futures import ProcessPoolExecutor as PPE
 from types import SimpleNamespace
@@ -22,45 +23,19 @@ class ITLW():
 		self.name = name
 		self.reflector_url = add_url_defaults(url, SimpleNamespace(port=5000, scheme='http'))
 		self.rpcs = {}
+		self.sio = None
 
 	def run(self):
 
-		# here we search through registered RPCs to find any top-level service nodes who's profile we send up to the reflector
+		self.sio = socketio.Client()
+		self.sio.connect(self.reflector_url)
 
-		endpoints = self.rpcs.keys()
-		# filter out the keys that end in __call__, since they're endpoints for RPCs, not get_profile
-		profile_endpoints = filter(lambda x : not x[::-1].split('/', 1)[0] == '__llac__', endpoints)
+		# this is the first message we'll send the reflector, containing the worker name
+		self.sio.emit('worker_header', data=str(self.name))
 
-		profile = {}
+		self.update_profile()
 
-		for pe in profile_endpoints:
-			parent = pe.split('/', 2)[1]
-
-			if parent in profile:
-				if (len(pe) < len(profile[parent])):
-					profile[parent] = pe
-
-			else:
-				profile[parent] = pe
-		
-		for service_name in profile:
-			endpoint = profile[service_name]
-			(fn, _) = self.rpcs[endpoint]
-			profile[service_name] = fn()
-
-		# remove the TLSN from the profile
-		del profile ['']
-
-		profile_str = serialize(profile)
-
-		sio = socketio.Client()
-		sio.connect(self.reflector_url)
-
-		# this is the first message we'll send the reflector, containing the service name and its profile
-		header_str = str(self.name)+'||'+profile_str
-		sio.emit('worker_header', data=header_str)
-
-		@sio.event
+		@self.sio.event
 		def rpc_request(req_str):
 			call_ID, endpoint, param_str = req_str.split('|', 3)
 
@@ -82,17 +57,23 @@ class ITLW():
 			chunk_size = 100_000
 
 			if (len(result_str) < chunk_size):
-				sio.emit('rpc_result', data=f'{call_ID}|{result_str}')
+				self.sio.emit('rpc_result', data=f'{call_ID}|{result_str}')
 
 			else:
 				num_chunks = ceil(len(result_str)/chunk_size)
 
 				for i in range(num_chunks):
 					chunk_str = result_str[ chunk_size*i : chunk_size*(i+1) ]
-					sio.emit('rpc_result_chunk', data=f'{str(i)}|{str(num_chunks)}|{call_ID}|{chunk_str}')
+					self.sio.emit('rpc_result_chunk', data=f'{str(i)}|{str(num_chunks)}|{call_ID}|{chunk_str}')
 
 		while True:
 			time.sleep(1_000_000)
+
+	def update_profile(self):
+		
+		tlsn = TLSNs[id(self)]
+		profile = tlsn.get_profile()
+		self.sio.emit('update_profile', data=serialize(profile))
 
 	def register_RPC(self, fn, endpoint, executor):
 
